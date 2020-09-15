03c47e26ee60efd3d1eb0128fe3dc0eee6e3bdc3
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-15 04:13:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,26 @@
 school = School.new("Bayside High School")
-school.roster
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+ 
